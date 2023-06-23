471db27c5a21c44cb9f6604aab2480555a02106a
Cristina Alvarez <alvarezcrisstinita@gmail.com>
2023-06-23 11:43:26 -0500

    1er ejercicio

---
+++ exercises_if.js
@@ -0,0 +1,59 @@
+var salirafura = true
+if (salirafuera) {console.log('llevar sombrilla');}
+
+var voydeviaje = true
+if (alaplaya) {console.log('llevar bloqueadir');}
+
+var mercar = true
+if (hacerunalista) {console.log('llevardinero');}
+
+var pasearKanela = true
+if (ponerlecorrea) {console.log('lllevarbolsa');}
+
+var piscina = true
+if (ponervestidodebaño) {console.log('usartapaoido');}
+
+var almorzar = true
+if (calentarcomida) {console.log('lavarlasmanos');}
+
+var caminar = true
+if (colocartenis) {console.log('llevaragua');}
+
+var piscina = true
+if (ponervestidodebaño) {console.log('usartapaoido');}
+
+var pagarcreditos = true
+if (revisarpago) {console.log('pagarenlinea');}
+
+var comprarcarro = true
+if (irconlaesposa) {console.log('llevardocumentos');}
+
+var estudiar = true
+if (hablarconlaprofesora) {console.log('esplicacionmedica');}
+
+var bailar = true
+if (vestirseelegante) {console.log('llevardinero');}
+
+var  motilarelesposo= true
+if (buscarlamaquina) {console.log('limppiar');}
+
+var  limpiargafas= true
+if (buscarellliquido) {console.log('limpiarconpañosuave');}
+
+var  celebrarfuesta= true
+if (buscarunlugar) {console.log('llevarcomidadeliciosa');}
+
+var  comprarcelular= true
+if (tenerpresupuesto) {console.log('elegirreferencia');}
+
+var  montarbicicleta= true
+if (revisarlasllantas) {console.log('llevarinflador');}
+
+var  comprarropa= true
+if (buscarlalmaceneseconomicos) {console.log('llevardinero');}
+
+var  cenar= true
+if (calentarcomida) {console.log('tomarmedicamentos');}
+
+var viajarapereira = true
+if (comprarloquenecesito) {console.log('empacartodo');}
