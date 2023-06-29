e4257497128ff6383058651e55d8e3bd8e714aee
Cristina Alvarez <alvarezcrisstinita@gmail.com>
2023-06-29 07:35:28 -0500

    feat: switch exercises

---
+++ switchExercise.js
@@ -0,0 +1,253 @@
+var caminar = 'salir';
+
+// switch
+switch(caminar){
+    case 'salir': {
+        console.log('llevartenis');
+    }; break;
+    case 'dormir': {
+        console.log('lavardientes');
+    }; break;
+    default: {
+        console.log('no esta organizada la accion');
+    }; break;
+}
+
+// variable tipo de dato boolean
+var caminar = true;
+
+switch(caminar){
+    case true: {
+        console.log('llevartenis');
+    }
+    default: {
+        console.log('no se si esta llevartenis');
+    }
+}
+
+
+var cocinar = 'lavarmanos';
+
+// switch
+switch(cocinar){
+    case 'cocinar': {
+        console.log('lavarlasmanos');
+    }; break;
+    case 'jugar': {
+        console.log('tomaragua');
+    }; break;
+    default: {
+        console.log('no esta organizada la accion');
+    }; break;
+}
+
+// variable tipo de dato boolean
+var cocinar = true;
+
+switch(cocinar){
+    case true: {
+        console.log('lavarlasmanos');
+    }
+    default: {
+        console.log('lavarlasmanos');
+    }
+}
+
+
+var verpeliculas = 'tenerpalomitas';
+
+// switch
+switch(verpeliculas){
+    case 'tenerpalomitas': {
+        console.log('preparar palomitas');
+    }; break;
+    case 'bailar': {
+        console.log('vestir elegante');
+    }; break;
+    default: {
+        console.log('no esta organizada la accion');
+    }; break;
+}
+
+// variable tipo de dato boolean
+var verpeliculas = true;
+
+switch(verpeliculas){
+    case true: {
+        console.log('preparar palomitas ');
+    }
+    default: {
+        console.log('no se si esta lloviendo');
+    }
+}
+
+var iraldoctor = 'llevar anotado sintomas';
+
+// switch
+switch (iraldoctor){
+   case 'ir al doctor': {
+     console.log ('llevar anotado sintomas');
+   }; break;
+   case 'ir a al supermercado' : {
+       console.log ('lleve abrigo');
+   }; break;
+     default: {
+           console.log ('no esta organisadalaaaccion');
+    }
+}
+// variable tipo de dato boolean
+ 
+var iraldoctorr = true;
+
+switch(iraldoctor){
+    case true: {
+        console.log('llevar anotado sintomas  ');
+    }
+    default: {
+        console.log('no se si esta lloviendo');
+    }
+}
+
+
+
+var llover = 'salir';
+
+// switch
+switch(llover){
+    case 'llover': {
+        console.log('llevar paraguas');
+    }; break;
+    case 'quedarencasa': {
+        console.log('ver peliculas');
+    }; break;
+    default: {
+        console.log('no esta organizada la accion');
+    }; break;
+}
+
+// variable tipo de dato boolean
+var llover = true;
+
+switch(llover){
+    case true: {
+        console.log('llevar paraguas');
+    }
+    default: {
+        console.log('no se si esta llevartenis');
+    }
+}
+
+
+
+var prepararensalada = 'lavar las verduras';
+
+// switch
+switch(prepararensalada){
+    case 'prepararensalada': {
+        console.log('lavar las verduras');
+    }; break;
+    case 'preparararroz': {
+        console.log('lavar el arroz');
+    }; break;
+    default: {
+        console.log('no esta organizada la accion');
+    }; break;
+}
+
+// variable tipo de dato boolean
+var prepararensaladar = true;
+
+switch(prepararensalada){
+    case true: {
+        console.log('lavar las verduras');
+    }
+    default: {
+        console.log('no se si esta llevartenis');
+    }
+}
+
+
+var iralaplaya = 'llevar traje de baño';
+
+// switch
+switch(iralaplaya){
+    case 'iralaplaya': {
+        console.log('llevar traje de baño');
+    }; break;
+    case 'irdecampamento': {
+        console.log('llevar repelente');
+    }; break;
+    default: {
+        console.log('no esta organizada la accion');
+    }; break;
+}
+
+// variable tipo de dato boolean
+var caminar = true;
+
+switch(iralaplaya){
+    case true: {
+        console.log('llevar traje de baño');
+    }
+    default: {
+        console.log('no se si esta llevartenis');
+    }
+}
+
+var jugar = 'cargar el celular';
+
+// switch
+switch(jugar){
+    case 'jugar': {
+        console.log('cargar el celular');
+    }; break;
+    case 'ver peliculas': {
+        console.log('comprar gaseosa');
+    }; break;
+    default: {
+        console.log('no esta organizada la accion');
+    }; break;
+}
+
+// variable tipo de dato boolean
+var jugar = true;
+
+switch(jugar){
+    case true: {
+        console.log('cargar el celular');
+    }
+    default: {
+        console.log('no se si esta llevartenis');
+    }
+}
+
+
+
+var bañarkanela = 'calentar agua';
+
+// switch
+switch(bañarkanela){
+  case 'bañarkanela': {
+    console.log('calentar agua');
+  };break;
+   case 'dardecomer': {
+     console.log('darle carne');
+   }; break; 
+   default: {
+       console.log('no esta organidasa la accion');
+   }break;
+}
+
+
+
+// variable tipo de dato boolean
+var bañarkanela = true;
+
+switch(bañarkanela){
+    case true: {
+        console.log('calentar agua');
+    }
+    default: {
+        console.log('no se si esta llevartenis');
+    }
+}
